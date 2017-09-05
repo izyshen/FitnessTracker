15813117ed42c02ff6f0254ba4ae5f21801941f6
Iris Shen <ishen.zy@gmail.com>
2017-09-04 23:41:52 -0400

    Second activity added

--- AndroidStudioProjects/FitnessTracker/app/src/main/java/com/ishen/fitnesstracker/Home.java
+++ AndroidStudioProjects/FitnessTracker/app/src/main/java/com/ishen/fitnesstracker/Home.java
@@ -1,13 +1,33 @@
 package com.ishen.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button bt1, bt2, bt3, bt4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bt1 = (Button) findViewById(R.id.button);
+        bt2 = (Button) findViewById(R.id.button2);
+        bt3 = (Button) findViewById(R.id.button3);
+        bt4 = (Button) findViewById(R.id.button4);
+
+        // takes user to workout activity
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent workoutIntent = new Intent(Home.this, WorkoutActivity.class);
+                startActivity(workoutIntent);
+            }
+
+        });
     }
 }
